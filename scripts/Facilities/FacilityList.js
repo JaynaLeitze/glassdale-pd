ad31c6ddfadec833732c0ebfad43b351d4f0e513
Jayna Leitze <jayna.leitze@gmail.com>
2020-11-12 12:03:12 -0600

    refactor facilitiesList & render

--- scripts/Facilities/FacilityList.js
+++ scripts/Facilities/FacilityList.js
@@ -1,30 +1,53 @@
+import { getCriminals, useCriminals } from "../criminals/CriminalProvider.js";
+import {
+  getCriminalFacilities,
+  useCriminalFacilities,
+} from "./CriminalFacilityProvider.js";
+import { renderFacilitiesButton } from "./FacilitiesButton.js";
 import { getFacilities, useFacilities } from "./FacilityDataProvider.js";
 import { FacilityHTML } from "./FacilityHTML.js";
 
 const eventHub = document.querySelector(".container");
 const facilitiesContainer = document.querySelector(".caseDataContainer");
 
+let facilities = [];
+let crimFac = [];
+let criminals = [];
+
 eventHub.addEventListener("facilitiesButtonClicked", () => {
   FacilityList();
 });
 
 export const FacilityList = () => {
-  getFacilities().then(() => {
-    const facilitiesArray = useFacilities();
-    renderFacilities(facilitiesArray);
-  });
+  getFacilities()
+    .then(getCriminalFacilities)
+    .then(getCriminals)
+    .then(() => {
+      facilities = useFacilities();
+      crimFac = useCriminalFacilities();
+      criminals = useCriminals();
+      renderFacilities();
+    });
 };
 
-const renderFacilities = (facilities) => {
-  let facilityHTMLRep = "";
-  for (const facility of facilities) {
-    facilityHTMLRep += FacilityHTML(facility);
-
-    facilitiesContainer.innerHTML = `
-       
-        
-        ${facilityHTMLRep}
-        
-    `;
-  }
+const renderFacilities = () => {
+  facilitiesContainer.innerHTML = `
+          
+            ${facilities
+              .map((facility) => {
+                const criminalRelationshipsForThisFacility = crimFac.filter(
+                  (cf) => cf.facilityId === facility.id
+                );
+                const criminalsAtThisFacility = criminalRelationshipsForThisFacility.map(
+                  (cf) => {
+                    const matchingCriminalObj = criminals.find(
+                      (criminal) => criminal.id === cf.criminalId
+                    );
+                    return matchingCriminalObj;
+                  }
+                );
+                return FacilityHTML(facility, criminalsAtThisFacility);
+              })
+              .join("")}
+        `;
 };
